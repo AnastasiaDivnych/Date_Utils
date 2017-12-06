1868d2624d6432f00c51059ce06c352e1aba10c7
AnastasiaDivnych <31613744+AnastasiaDivnych@users.noreply.github.com>
2017-12-06 16:46:02 +0100

    Update Metodi.java

--- Metodi.java
+++ Metodi.java
@@ -1,7 +1,7 @@
 package Es;
 
 import java.util.Date;
-
+@SuppressWarnings("deprecation")
 public class Metodi {
 	
 	public static void DifferenzaSecondi (Date date , Date date2) {
@@ -23,15 +23,12 @@ public class Metodi {
 	}
 	
 	public static void DifferenzaAnni (Date date , Date date2) {
-		@SuppressWarnings("deprecation")
 		int diffYear = date2.getYear() - date.getYear();
 		System.out.println("In anni : " + diffYear);
 	}
 	
 	public static void DifferenzaMesi (Date date , Date date2) {
-		@SuppressWarnings("deprecation")
 		int year = date2.getYear() - date.getYear();
-	    @SuppressWarnings("deprecation")
 		int diffInMounth = year * 12 + date2.getMonth() - date.getMonth();
 	    System.out.println("In mesi : " + diffInMounth);
 	}
@@ -41,26 +38,21 @@ public class Metodi {
 		int diffInDays = (int) (diff / (1000 * 60 * 60 * 24));
 		System.out.println("In giorni : " + diffInDays);
 	}
-	
-	@SuppressWarnings("deprecation")
 	public static void AggiungereSecondi (Date date , int secondi) {
 		long add = date.getSeconds() + secondi;
 		date.setSeconds((int) add);
 		System.out.println(date);
 	}
-	@SuppressWarnings("deprecation")
 	public static void AggiungereMinuti (Date date , int minuti) {
 		long add = date.getMinutes() + minuti;
 		date.setMinutes((int) add);
 		System.out.println(date);
 	}
-	@SuppressWarnings("deprecation")
 	public static void AggiungereOre (Date date , int ore) {
 		long add = date.getHours() + ore;
 		date.setHours((int) add);
 		System.out.println(date);
 	}
-	@SuppressWarnings("deprecation")
 	public static void AggiungereGiorni (Date date , int giorni) {
 		long add = (date.getDate() )+ giorni;
 		date.setDate((int) add);
@@ -72,17 +64,13 @@ public class Metodi {
 		date.setTime(add);
 		System.out.println(date);
 	}
-	@SuppressWarnings("deprecation")
 	public static void AggiungereAnni (Date date , int anni) {
 		long add = date.getYear() + anni;
 		date.setYear((int) add);
 		System.out.println(date);
 	}
-	
 	public static void main (String[] args) {
-		@SuppressWarnings("deprecation")
 		Date d = new Date(2017 - 1900,10,3,9,29,52);
-		@SuppressWarnings("deprecation")
 		Date d2 = new Date (2017 - 1900,12,4,9,29,58);
 		
 		DifferenzaAnni(d, d2);
